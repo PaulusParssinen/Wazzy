30e15beb6039ed73a0541559357ccf772418eee4
ArachisH <iKBenPeanut@gmail.com>
2020-11-11 17:30:46 -0800

    * This value should be read/written as a long

--- Wazzy/Bytecode/Instructions/Numeric/ConstantI64Ins.cs
+++ Wazzy/Bytecode/Instructions/Numeric/ConstantI64Ins.cs
@@ -1,18 +1,25 @@
-﻿using Wazzy.IO;
+﻿using System.Collections.Generic;
+
+using Wazzy.IO;
 
 namespace Wazzy.Bytecode.Instructions.Numeric
 {
     public class ConstantI64Ins : WASMInstruction
     {
-        public int Constant { get; set; }
+        public long Constant { get; set; }
 
         public ConstantI64Ins(ref WASMReader input)
-            : this(input.ReadIntLEB128())
+            : this(input.ReadLongLEB128())
         { }
-        public ConstantI64Ins(int constant = 0)
+        public ConstantI64Ins(long constant = 0)
             : base(OPCode.ConstantI64)
         {
             Constant = constant;
+        }
+
+        public override void Execute(Stack<object> stack, WASMModule context)
+        {
+            stack.Push(Constant);
         }
 
         protected override void WriteBodyTo(ref WASMWriter output)
