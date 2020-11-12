41cc88d6daa5bf24b853ed0d065e5c84720c708a
ArachisH <iKBenPeanut@gmail.com>
2020-11-11 17:31:21 -0800

    * Do the switching thing

--- Wazzy/Types/WASMType.cs
+++ Wazzy/Types/WASMType.cs
@@ -6,14 +6,21 @@ namespace Wazzy.Types
 {
     public abstract class WASMType : WASMObject
     {
-        private static readonly Type[] _supportedTypes;
-
-        static WASMType()
+        public static Type GetValueType(byte valueTypeId) => valueTypeId switch
         {
-            _supportedTypes = new[] { typeof(int), typeof(long), typeof(float), typeof(double) };
-        }
-
-        public static Type GetType(int valueTypeId) => _supportedTypes[127 - valueTypeId];
-        public static bool IsSupportedType(int valueTypeId) => valueTypeId >= 124 && valueTypeId <= 127;
+            0x7F => typeof(int),
+            0x7E => typeof(long),
+            0x7D => typeof(float),
+            0x7C => typeof(double),
+            _ => throw new ArgumentException($"The provided {nameof(valueTypeId)}({valueTypeId}, 0x{valueTypeId:X2}) is currently not supported in the WebAssembly specificiation.")
+        };
+        public static byte GetValueTypeId(Type valueType) => Type.GetTypeCode(valueType) switch
+        {
+            TypeCode.Int32 => 0x7F,
+            TypeCode.Int64 => 0x7E,
+            TypeCode.Single => 0x7D,
+            TypeCode.Double => 0x7C
+        };
+        public static bool IsSupportedValueTypeId(byte valueTypeId) => (valueTypeId >= 124 && valueTypeId <= 127);
     }
 }
