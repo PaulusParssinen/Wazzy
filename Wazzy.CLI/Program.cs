d442e5332f12094dc69cf3bc990c84599bd63198
ArachisH <iKBenPeanut@gmail.com>
2020-11-11 21:43:44 -0800

    * Hope this fixes VS acting weird

--- Wazzy.CLI/Program.cs
+++ Wazzy.CLI/Program.cs
@@ -5,13 +5,24 @@ namespace Wazzy.CLI
 {
     public class Program
     {
-        public static void Main(string[] args) => new Program().Run(args);
-
-        private void Run(string[] args)
+        public static void Main(string[] args)
         {
-            byte[] moduleData = File.ReadAllBytes(args[0]);
-            var module = new WASMModule(moduleData);
-            module.Disassemble();
+            byte[] originalModuleData = File.ReadAllBytes(args[0]);
+            var originalModule = new WASMModule(originalModuleData);
+            originalModule.Disassemble();
+
+            byte[] assembledModuleData = originalModule.ToArray();
+
+            string modifiedPath = Path.GetFullPath(args[0]).Replace("original_", string.Empty);
+            File.WriteAllBytes(modifiedPath, assembledModuleData);
+
+            for (int i = 4; i < Math.Min(originalModuleData.Length, assembledModuleData.Length); i++)
+            {
+                if (originalModuleData[i] == assembledModuleData[i]) continue;
+                string originalChunkHex = BitConverter.ToString(originalModuleData, i, 10);
+                string modifiedChunkHex = BitConverter.ToString(assembledModuleData, i, 10);
+                System.Diagnostics.Debugger.Break();
+            }
             Console.ReadLine();
         }
     }
