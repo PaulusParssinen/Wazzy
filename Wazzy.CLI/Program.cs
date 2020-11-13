0d2c3be3adc0d41bf7a3b0ac3b0a74a1b1fddacc
ArachisH <iKBenPeanut@gmail.com>
2020-11-12 19:11:04 -0800

    Stole this, thanks g

--- Wazzy.CLI/Program.cs
+++ Wazzy.CLI/Program.cs
@@ -1,28 +1,47 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
 
 namespace Wazzy.CLI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             byte[] originalModuleData = File.ReadAllBytes(args[0]);
             var originalModule = new WASMModule(originalModuleData);
+
+            var disassembleTime = Stopwatch.StartNew();
             originalModule.Disassemble();
+            disassembleTime.Stop();
 
-            byte[] assembledModuleData = originalModule.ToArray();
+            string assembledPath = args[0].Replace("original_", string.Empty);
+            using var fs = File.Create(assembledPath);
+             var writer = PipeWriter.Create(fs);
 
-            string modifiedPath = Path.GetFullPath(args[0]).Replace("original_", string.Empty);
-            File.WriteAllBytes(modifiedPath, assembledModuleData);
+            var assembleTime = Stopwatch.StartNew();
+            originalModule.Assemble(writer);
+            await writer.FlushAsync();
+            assembleTime.Stop();
+            await fs.DisposeAsync();
 
+            Console.WriteLine($"Assemble: " + assembleTime.Elapsed.ToString("mm\\:ss\\.ff"));
+            Console.WriteLine($"Disassemble: " + disassembleTime.Elapsed.ToString("mm\\:ss\\.ff"));
+
+            bool areModulesEqual = true;
+            byte[] assembledModuleData = await File.ReadAllBytesAsync(assembledPath);
             for (int i = 4; i < Math.Min(originalModuleData.Length, assembledModuleData.Length); i++)
             {
                 if (originalModuleData[i] == assembledModuleData[i]) continue;
                 string originalChunkHex = BitConverter.ToString(originalModuleData, i, 10);
                 string modifiedChunkHex = BitConverter.ToString(assembledModuleData, i, 10);
+                areModulesEqual = false;
                 System.Diagnostics.Debugger.Break();
             }
+            Console.WriteLine("Are Modules Equal: " + areModulesEqual);
+            Console.ReadLine();
         }
     }
 }
