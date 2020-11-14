0f9679d139419b3878db8e53c8b4fcbf5ccc364c
ArachisH <iKBenPeanut@gmail.com>
2020-11-13 17:04:47 -0800

    * Allow an inheriting class to know when a subsection has been added, removed, and cleared from a list

--- Wazzy/Sections/WASMSectionEnumerable.cs
+++ Wazzy/Sections/WASMSectionEnumerable.cs
@@ -13,22 +13,58 @@ namespace Wazzy.Sections
             Subsections = new List<T>();
         }
 
+        protected virtual void SubsectionsCleared()
+        { }
+        protected virtual void SubsectionAdded(T subsection)
+        { }
+        protected virtual void SubsectionRemoved(T subsection)
+        { }
+
         #region IList<T> Implementation
         public T this[int index]
         {
             get => Subsections[index];
-            set => Subsections[index] = value;
+            set
+            {
+                Subsections[index] = value;
+                SubsectionRemoved(value);
+            }
         }
         public int Count => Subsections.Count;
         bool ICollection<T>.IsReadOnly => false;
 
+        public void RemoveAt(int index)
+        {
+            T subsection = Subsections[index];
+            Subsections.RemoveAt(index);
+            SubsectionRemoved(subsection);
+        }
+        public void Insert(int index, T item)
+        {
+            Subsections.Insert(index, item);
+            SubsectionAdded(item);
+        }
         public int IndexOf(T item) => Subsections.IndexOf(item);
-        public void RemoveAt(int index) => Subsections.RemoveAt(index);
-        public void Insert(int index, T item) => Subsections.Insert(index, item);
 
-        public void Clear() => Subsections.Clear();
-        public void Add(T item) => Subsections.Add(item);
-        public bool Remove(T item) => Subsections.Remove(item);
+        public void Clear()
+        {
+            Subsections.Clear();
+            SubsectionsCleared();
+        }
+        public void Add(T item)
+        {
+            Subsections.Add(item);
+            SubsectionAdded(item);
+        }
+        public bool Remove(T item)
+        {
+            bool removed = Subsections.Remove(item);
+            if (removed)
+            {
+                SubsectionRemoved(item);
+            }
+            return removed;
+        }
         public bool Contains(T item) => Subsections.Contains(item);
         public void CopyTo(T[] array, int arrayIndex) => Subsections.CopyTo(array, arrayIndex);
 
