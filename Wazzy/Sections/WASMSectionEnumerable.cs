323120fc6e9d15d0570e6ff1b81d70fa1805de30
ArachisH <iKBenPeanut@gmail.com>
2020-11-15 16:38:39 -0800

    * Create a cache of indices for all sections that inherit from this class, and use a reference type as a subsection item * Create virtual methods for inheriting classes to utilize when modifiyng the internal subsection list. This allows for custom sorting/caching of information per section.

--- Wazzy/Sections/WASMSectionEnumerable.cs
+++ Wazzy/Sections/WASMSectionEnumerable.cs
@@ -5,71 +5,129 @@ namespace Wazzy.Sections
 {
     public abstract class WASMSectionEnumerable<T> : WASMSection, IList<T>
     {
-        protected List<T> Subsections { get; }
+        private readonly List<T> _subsections;
+        private readonly Dictionary<T, int> _indices;
+
+        protected int Capacity
+        {
+            get => _subsections.Capacity;
+            set
+            {
+                _subsections.Capacity = value;
+                if (_indices != null)
+                {
+                    _indices.EnsureCapacity(value);
+                }
+            }
+        }
 
         protected WASMSectionEnumerable(WASMSectionId id)
             : base(id)
         {
-            Subsections = new List<T>();
+            _subsections = new List<T>();
+            if (!typeof(T).IsValueType)
+            {
+                // These objects can't be trusted, as duplicate values may appear.
+                _indices = new Dictionary<T, int>();
+            }
         }
 
-        protected virtual void SubsectionsCleared()
+        protected virtual void Cleared()
         { }
-        protected virtual void SubsectionAdded(T subsection)
+        protected virtual void Added(int index, T subsection)
         { }
-        protected virtual void SubsectionRemoved(T subsection)
+        protected virtual void Removed(int index, T subsection)
         { }
 
         #region IList<T> Implementation
         public T this[int index]
         {
-            get => Subsections[index];
+            get => _subsections[index];
             set
             {
-                Subsections[index] = value;
-                SubsectionRemoved(value);
+                T oldSubsection = _subsections[index];
+                _subsections[index] = value;
+
+                Removed(index, oldSubsection);
+                Added(index, value);
             }
         }
-        public int Count => Subsections.Count;
+        public int Count => _subsections.Count;
         bool ICollection<T>.IsReadOnly => false;
 
+        public int Add(T item)
+        {
+            int index = _subsections.Count;
+            _subsections.Add(item);
+            if (_indices != null)
+            {
+                _indices.Add(item, index);
+            }
+            Added(index, item);
+            return index;
+        }
+        void ICollection<T>.Add(T item) => Add(item);
+
+        public int Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index != -1)
+            {
+                RemoveAt(index);
+            }
+            return index;
+        }
+        bool ICollection<T>.Remove(T item) => Remove(item) != -1;
+
+        public int IndexOf(T item)
+        {
+            if (_indices == null)
+            {
+                return _subsections.IndexOf(item);
+            }
+            else return _indices.TryGetValue(item, out int index) ? index : -1;
+        }
         public void RemoveAt(int index)
         {
-            T subsection = Subsections[index];
-            Subsections.RemoveAt(index);
-            SubsectionRemoved(subsection);
+            T subsection = _subsections[index];
+            _subsections.RemoveAt(index);
+            if (_indices != null)
+            {
+                _indices.Remove(subsection);
+                for (int i = index; i < _subsections.Count; i++)
+                {
+                    T a = _subsections[i];
+                    _indices[a]--;
+                }
+            }
+            Removed(index, subsection);
         }
         public void Insert(int index, T item)
         {
-            Subsections.Insert(index, item);
-            SubsectionAdded(item);
+            _subsections.Insert(index, item);
+            if (_indices != null)
+            {
+                _indices.Add(item, index);
+                for (int i = index + 1; i < _subsections.Count; i++)
+                {
+                    T subsection = _subsections[i];
+                    _indices[subsection]++;
+                }
+            }
+            Added(index, item);
         }
-        public int IndexOf(T item) => Subsections.IndexOf(item);
 
         public void Clear()
         {
-            Subsections.Clear();
-            SubsectionsCleared();
+            _subsections.Clear();
+            _indices.Clear();
+            Cleared();
         }
-        public void Add(T item)
-        {
-            Subsections.Add(item);
-            SubsectionAdded(item);
-        }
-        public bool Remove(T item)
-        {
-            bool removed = Subsections.Remove(item);
-            if (removed)
-            {
-                SubsectionRemoved(item);
-            }
-            return removed;
-        }
-        public bool Contains(T item) => Subsections.Contains(item);
-        public void CopyTo(T[] array, int arrayIndex) => Subsections.CopyTo(array, arrayIndex);
+        public bool Contains(T item) => IndexOf(item) != -1;
+        public void CopyTo(T[] array, int arrayIndex) => _subsections.CopyTo(array, arrayIndex);
 
-        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Subsections).GetEnumerator();
-        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)Subsections).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_subsections).GetEnumerator();
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)_subsections).GetEnumerator();
         #endregion
     }
 }
