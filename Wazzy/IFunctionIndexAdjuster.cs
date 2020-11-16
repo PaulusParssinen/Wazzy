2cb6bbd8151bbaebecd9b0e9e543a053fcfa6b37
ArachisH <iKBenPeanut@gmail.com>
2020-11-15 17:49:54 -0800

    * Create an interface that can be used to identify the starting point of all function indices when an import has been modified, or a function has been added

---
+++ Wazzy/IFunctionIndexAdjuster.cs
@@ -0,0 +1,10 @@
+﻿using Wazzy.Sections;
+
+namespace Wazzy
+{
+    public interface IFunctionIndexAdjuster
+    {
+        public uint GetFunctionIndexOffset() => 0;
+        public uint GetFunctionIndexOffset(ImpexDesc description) => description == ImpexDesc.Function ? GetFunctionIndexOffset() : 0;
+    }
+}
