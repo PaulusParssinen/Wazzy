04bff1425e5bda4908df6e233d10eefc278785d9
ArachisH <iKBenPeanut@gmail.com>
2020-11-15 16:44:57 -0800

    * Allow the abilty to easily add new function imports, where every new function added is imported BEFORE the the first 'original' function import. This allows us to later append +N to every object that utilizes a functionIndex without having to check for the threshhold of where that index was changed

--- Wazzy/Sections/ImportSection.cs
+++ Wazzy/Sections/ImportSection.cs
@@ -1,25 +1,76 @@
-﻿using Wazzy.IO;
+﻿using System.Collections.Generic;
+
+using Wazzy.IO;
 using Wazzy.Sections.Subsections;
 
 namespace Wazzy.Sections
 {
     public class ImportSection : WASMSectionEnumerable<ImportSubsection>
     {
-        public ImportSection(ref WASMReader input)
+        private readonly List<ImportSubsection> _functionImports;
+        private readonly List<ImportSubsection> _freshlyImportedFunctions;
+
+        public IReadOnlyList<ImportSubsection> FreshlyImportedFunctions { get; }
+
+        public ImportSection()
             : base(WASMSectionId.ImportSection)
         {
-            Subsections.Capacity = (int)input.ReadIntULEB128();
-            for (int i = 0; i < Subsections.Capacity; i++)
+            _functionImports = new List<ImportSubsection>();
+            _freshlyImportedFunctions = new List<ImportSubsection>();
+
+            FreshlyImportedFunctions = _freshlyImportedFunctions.AsReadOnly();
+        }
+        public ImportSection(ref WASMReader input)
+            : this()
+        {
+            Capacity = (int)input.ReadIntULEB128();
+            for (int i = 0; i < Capacity; i++)
             {
                 Add(new ImportSubsection(ref input));
+            }
+        }
+
+        public uint Add(string module, string name, uint functionTypeIndex)
+        {
+            int functionImportIndex = FreshlyImportedFunctions.Count;
+            var freshFunctionImport = new ImportSubsection(module, name, functionTypeIndex);
+
+            _freshlyImportedFunctions.Add(freshFunctionImport);
+            Insert(IndexOf(_functionImports[0]) + functionImportIndex, freshFunctionImport);
+
+            return (uint)functionImportIndex;
+        }
+
+        protected override void Cleared()
+        {
+            _functionImports.Clear();
+        }
+        protected override void Added(int index, ImportSubsection subsection)
+        {
+            if (subsection.Description != ImpexDesc.Function) return;
+            if (!_freshlyImportedFunctions.Contains(subsection))
+            {
+                _functionImports.Add(subsection);
+            }
+            else _functionImports.Insert(_freshlyImportedFunctions.Count - 1, subsection);
+        }
+        protected override void Removed(int index, ImportSubsection subsection)
+        {
+            if (subsection.Description == ImpexDesc.Function)
+            {
+                _functionImports.Remove(subsection);
+                if (_freshlyImportedFunctions.Contains(subsection))
+                {
+                    _freshlyImportedFunctions.Remove(subsection);
+                }
             }
         }
 
         protected override int GetBodySize()
         {
             int size = 0;
-            size += WASMReader.GetULEB128Size((uint)Subsections.Count);
-            foreach (ImportSubsection import in Subsections)
+            size += WASMReader.GetULEB128Size((uint)Count);
+            foreach (ImportSubsection import in this)
             {
                 size += import.GetSize();
             }
@@ -27,8 +78,8 @@ namespace Wazzy.Sections
         }
         protected override void WriteBodyTo(ref WASMWriter output)
         {
-            output.WriteULEB128((uint)Subsections.Count);
-            foreach (ImportSubsection import in Subsections)
+            output.WriteULEB128((uint)Count);
+            foreach (ImportSubsection import in this)
             {
                 import.WriteTo(ref output);
             }
